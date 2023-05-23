affb7002799b6c1b94f79689b2b0d7f89681c4bf
Kemila <kemiladabare9@gmail.com>
2023-05-23 21:37:07 +0530

    mongodb connected

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from "express";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.listen(3001, () => console.log("server running successfuly"))
+mongoose.connect(
+  "mongodb+srv://test:test@cluster0.k0btplj.mongodb.net/Recipes?retryWrites=true&w=majority"
+);
+
+app.listen(3001, () => console.log("server running successfuly"));
