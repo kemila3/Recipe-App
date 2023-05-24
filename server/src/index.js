1a91f352cdc3eb579b54b30b9be17619c0dbc07c
Kemila <kemiladabare9@gmail.com>
2023-05-24 21:41:30 +0530

    routes added

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose, { mongo } from "mongoose";
+import { userRouter } from "./routes/userRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/auth', userRouter )
 
 mongoose.connect(
   "mongodb+srv://test:test@cluster0.k0btplj.mongodb.net/Recipes?retryWrites=true&w=majority"
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,3 +3,7 @@ import jwt from "jsonwebtoken"
 import bcrypt from 'bcrypt'
 
 const router = express.Router()
+
+
+
+export {router as userRouter};
