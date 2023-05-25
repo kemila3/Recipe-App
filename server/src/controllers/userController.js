e4a3e91d7b451bb5096454ecc94db45eb0b96b1b
Kemila <kemiladabare9@gmail.com>
2023-05-25 23:32:27 +0530

    register a user using POST method done

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,11 +4,25 @@ import bcrypt from "bcrypt";
 import { mongo } from "mongoose";
 import { User } from "../models/userModel.js";
 
+// POST method to register a user
 export const registerUser = async (req, res) => {
   const { username, password } = req.body;
 
   const user = await User.findOne({ username });
-  res.status(200).json(user);
+  if (user) {
+    res.status(200).json({ message: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+  res.status(200).json({ newUser, message: "New user registered" });
 };
 
-export const loginUser = async (req, res) => {};
+export const loginUser = async (req, res) => {
+    const user = await User.findOne({username})
+
+    if(user){
+        res.status(500).json({message: "User already exists"})
+    }
+    res.status()
+};
--- server/src/index.js
+++ server/src/index.js
@@ -2,17 +2,16 @@ import express from "express";
 import cors from "cors";
 import mongoose, { mongo } from "mongoose";
 import { userRouter } from "./routes/userRoutes.js";
-import {connectDB} from '../src/config/dbConnection.js'
-import dotenv from 'dotenv'
+import { connectDB } from "../src/config/dbConnection.js";
+import dotenv from "dotenv";
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/auth', userRouter)
-
+app.use("/auth", userRouter);
 
 app.listen(3001, () => console.log("server running successfuly"));
