c57a0893496d4f3da7173cf04e956324adea2144
Kemila <kemiladabare9@gmail.com>
2023-06-06 05:09:03 +0530

    Login done

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -9,8 +9,6 @@ export const Auth = () => {
   );
 };
 
-const Login = () => {};
-
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -38,6 +36,39 @@ const Register = () => {
           />
         </div>
         <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="auth-container">
+      <form>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
