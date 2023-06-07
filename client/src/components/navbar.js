ef06d453195f66789798e25b16e6fc0680c41414
Kemila <kemiladabare9@gmail.com>
2023-06-07 19:45:11 +0530

    Login succesfull using useCookies state

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,15 @@
+import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"])
+
+
     return <div className="navbar">
         <Link to="/">Home</Link>
         <Link to="/create-recipe">Create Recipe</Link>
         <Link to="/save-recipes">Saved Recipes</Link>
-        <Link to="/auth">Register/Login</Link>
+        {!cookies.access_token ? (  <Link to="/auth">Register/Login</Link>) : <button> Logout</button>}
+      
     </div>
 }
